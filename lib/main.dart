3284b3b3a25ad9456c84c184bf5df00f7f78b550
stickman1257 <stickman1257@naver.com>
2020-07-28 23:31:37 +0900

    권승오 첫커밋

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: '플러터'),
+      home: MyHomePage(title: '권승오'),
     );
   }
 }
